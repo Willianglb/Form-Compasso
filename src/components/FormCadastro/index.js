a7ccdf4f76c189cad953d68de7aae01d23986678
Willian Gabriel <willian_glbotelho@hotmail.com>
2021-04-20 16:20:33 -0300

    :bug:

--- src/components/FormCadastro/index.js
+++ src/components/FormCadastro/index.js
@@ -2,133 +2,72 @@ import React, {useState, useContext} from 'react';
 import Context, { appContext } from '../../components/Store/Context';
 import * as S from './FormCadastro.styles';
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
 import { button, number, select } from '@storybook/addon-knobs';
 
+const schema = yup.object().shape({
+    nome: yup.string().required("O campo nome é obrigatório."),
+    email: yup.string().email().required("O campo email é obrigatório."),
+    senha: yup.string().required("O campo senha é obrigatório.")
+});
+
 const FormCadastro = (props) => {
-    const {nome, email, senha, setNome, setEmail, setSenha} = useContext(appContext);
-    const {tema} = useState();
-    const [bordeEmail, setbordeEMail] = useState();
-    const [bordeSenha, setbordeSenha] = useState();
-    const [bordeNome, setbordeNome] = useState();
-    const { formState: { errors } } = useForm();
+    const {nome, email, senha} = useContext(appContext);
 
-    function validar() {
-        if (email.length > 0) {
-            setbordeEMail(false);
-        }
-        else {
-            setbordeEMail(true);
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        resolver: yupResolver(schema)
+    });
 
-        }
-
-        if (senha.length > 5) {
-            setbordeSenha(false);
-        } 
-        else {
-            setbordeSenha(true);
-        }
-
-        if (nome.length > 0) {
-            setbordeNome(false);
-
-        } 
-        else {
-            setbordeNome(true);
-        }
-
-    }
-
-
-    function validateEmail(email) {
-        var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-        if (reg.test(email)) {
-    }
-}
+    const onSubmit = (data) => console.log(data);
 
     return (
         <>
         <S.Box>
-            <S.Form onSubmit={(event) => {
-                event.preventDefault();
-                props.aoEnviar({ nome, email, senha })
-                setSenha("");
-                setEmail("");
-                setNome("");
-                
-            }}>
-                <S.Select onChange={(event) => {
-                    event.preventDefault();
-                    props.enviarTema({tema})
-                }}>
-                <select name="select" onChange >
-                    <option value="cinza">Cinza</option>
-                    <option value="preto">Preto</option>
-                    <option value="azul">Azul</option>
-                </select>
-                </S.Select>
-
+            <S.Form onSubmit={handleSubmit(onSubmit)}>
                 <S.InputTitulo>Cadastro</S.InputTitulo>
                 <S.Label>Nome completo</S.Label>
-                <S.Input isActive={bordeNome}
-                value={nome}
-                type="text"
-                required
-                placeholder="Seu Nome"
-                onChange={(event) => {
-                    setNome(event.target.value);
-                }}
-                onBlur={() => {
-                    validar();
-                }}
-                id="nome"
-                label="nome"
-                variant="outlined"
-                margin="normal"
-                fullWidth
+                <S.Input
+                    {...register("nome")}
+                    value={nome}
+                    type="text"
+                    required
+                    placeholder="Seu Nome"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
                 />
-                {errors.nome && <p>Insira seu primeiro nome.</p>}
+                {errors.nome?.message}
 
                 <S.Label>Email</S.Label>
-                <S.Input isActive={bordeEmail}
-                value={email}
-                type="email"
-                required
-                placeholder="Seu Email"
-                onChange={(event) => {
-                    setEmail(event.target.value);
-                }}
-                onBlur={() => {
-                    validar();
-                    validateEmail(email);
-                }}
-                id="email"
-                label="email"
-                variant="outlined"
-                margin="normal"
-                fullWidth
+                <S.Input
+                    {...register("email")}
+                    value={email}
+                    type="email"
+                    required
+                    placeholder="Seu Email"
+                    id="email"
+                    label="email"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
                 />
-                {errors.email && <p>Insira seu sobrenome</p>}
+                {errors.email?.message}
 
                 <S.Label>Senha</S.Label>
-                <S.Input isActive={bordeSenha}
-                value={senha}
-                type="password"
-                onChange={(event) => {
-                    setSenha(event.target.value);
-
-                }}
-                onBlur={() => {
-                    validar();
-                }}
-                id="password"
-                required
-                placeholder="Sua senha"
-                label="password"
-                variant="password"
-                margin="password"
-                fullWidth
+                <S.Input
+                    {...register("senha")}
+                    value={senha}
+                    type="password"
+                    id="password"
+                    required
+                    placeholder="Sua senha"
+                    label="password"
+                    variant="password"
+                    margin="password"
+                    fullWidth
                 />
-                {errors.senha && <p>Insira sua senha, por favor.</p>}
+                {errors.senha?.message}
                 
                 <S.InputButton type="submit" variant="contained" />
             </S.Form>
